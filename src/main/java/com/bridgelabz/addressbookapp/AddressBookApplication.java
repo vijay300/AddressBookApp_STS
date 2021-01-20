6d17281643510a427a935713545a97f354919e3d
vijay300 <gnvijayk@gmail.com>
2021-01-20 14:51:28 +0530

    [Vijay] Refactored: properties as dev and prod and changed the database details for username and password

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookApplication.java
@@ -7,11 +7,12 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
-public class AddressBookAppApplication {
+public class AddressBookApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
         log.info("Address Book App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Address Book DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
